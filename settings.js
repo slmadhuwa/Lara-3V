963244f149960177a022b050a90a70cd40eeed4e
Sl madhuwa <88928383+slmadhuwa@users.noreply.github.com>
2025-10-17 07:23:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "TdZF0TbY#iGf4WE7svnnu-ZReJZcfa3CSVlb9MF6GhngBD5sr1xQ", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
-DEV: process.env.DEV || "94779062397",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94742274409",
+DEV: process.env.DEV || "94742274409",
 LANG: process.env.LANG || "EN", // EN | SI
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
